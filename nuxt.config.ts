aa8733e40604ee7e964f8e86fd30af2e814baeee
Landen English <31443939+landenenglish@users.noreply.github.com>
2025-10-19 11:21:00 -0400

    Fix mobile safari

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,8 @@ export default defineNuxtConfig({
       meta: [
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0',
+          content:
+            'width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no',
         },
       ],
       script: [
